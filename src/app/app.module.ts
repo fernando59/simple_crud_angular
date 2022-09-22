f78ef57932ab8ebbf8126e85061f3caceaa6dc6f
Fernando <fernandomercado529@gmail.com>
2022-09-22 12:13:13 -0400

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PersonListComponent } from './components/persons/person-list/person-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PersonListComponent
   ],
   imports: [
     BrowserModule,
