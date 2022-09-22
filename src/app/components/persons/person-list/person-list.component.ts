0832b2ce02c04ee2815fe12be1306aa055666c7b
Fernando <fernandomercado529@gmail.com>
2022-09-22 13:07:33 -0400

    add design form

--- src/app/components/persons/person-list/person-list.component.ts
+++ src/app/components/persons/person-list/person-list.component.ts
@@ -1,14 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Person } from 'src/app/models/person.model';
 
 @Component({
-  selector: 'app-person-list',
+  selector: 'person-list',
   templateUrl: './person-list.component.html',
-  styleUrls: ['./person-list.component.css']
 })
 export class PersonListComponent implements OnInit {
 
   constructor() { }
-
+  @Input() persons: Person[] = [];
   ngOnInit(): void {
   }
 
